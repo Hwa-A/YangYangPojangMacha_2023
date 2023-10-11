e7f7bd5e0c9dc9719f0fa662d292ec48fb985778
naeunYang <didskdms2@naver.com>
2023-10-11 10:05:55 +0900

    나은123 txt

--- YangPojang/app/src/main/java/com/yuhan/yangpojang/home/HttpResponse.java
+++ YangPojang/app/src/main/java/com/yuhan/yangpojang/home/HttpResponse.java
@@ -48,7 +48,7 @@ public class HttpResponse {
                 if (response.isSuccessful()) {
                     try {
                         String body = response.body().string(); //HTTP응답의 본문을 문자열로 읽어옴
-
+                        Log.d("HttpResponse", "리스폰스된 body : " + body);
                         JSONObject jsonObject = new JSONObject(body); //문자열 형태로 읽어온 body를 JSON형식으로 파싱하여 JSONObject객체로 변환 -> JSON데이터 구조를 유지한 채로 데이터 접근 가능
 
                         // "contents"객체 안에 있는 "poi" 배열을 가져옴
@@ -58,29 +58,34 @@ public class HttpResponse {
 
                         for (int i = 0; i < poiArray.length(); i++) {
                             JSONObject poiObject = poiArray.getJSONObject(i); //poiArray의 데이터 항목을 JSONObject로 추출 -> 각 데이터 항목의 필드에 접근
+                            Log.d("HttpResponse", "리스폰스된 poiObject : ");
+                            String typeName = poiObject.getString("typeName"); //분류
 
-                            String name = poiObject.getString("name"); // 지명
-                            String roadAdd = poiObject.getString("roadAdres"); // 도로명
-                            String jibunAdd = poiObject.getString("jibunAdres"); // 지번
-                            String latitude = poiObject.getString("x"); // x좌표
-                            String longitude = poiObject.getString("y"); // y좌표
+                            if(!typeName.contains("버스정류장")){
+                                Log.d("HttpResponse", "리스폰스된 poiObject(without버정) : " + poiObject);
+                                String name = poiObject.getString("name"); // 지명
+                                String roadAdd = poiObject.getString("roadAdres"); // 도로명
+                                String jibunAdd = poiObject.getString("jibunAdres"); // 지번
+                                String latitude = poiObject.getString("x"); // x좌표
+                                String longitude = poiObject.getString("y"); // y좌표
 
-                            if (jibunAdd.endsWith("도") || jibunAdd.endsWith("대")) {
-                                // "도" 글자가 있는 경우, 맨 끝의 "도" 글자를 제외한 부분을 추출
-                                jibunAdd = jibunAdd.substring(0, jibunAdd.length() - 1);
-                            }
-
-
-                            if (name.contains(keyword) || roadAdd.contains(keyword) || jibunAdd.contains(keyword)) {
-                                String address;
-
-                                if (roadAdd.equals(" ")) {
-                                    address = jibunAdd + "/" + latitude + "/" + longitude;
-                                } else {
-                                    address = roadAdd + "/" + latitude + "/" + longitude;
+                                if (jibunAdd.endsWith("도") || jibunAdd.endsWith("대")) {
+                                    // "도" 글자가 있는 경우, 맨 끝의 "도" 글자를 제외한 부분을 추출
+                                    jibunAdd = jibunAdd.substring(0, jibunAdd.length() - 1);
                                 }
 
-                                address_hash.put(name, address);
+
+                                if (name.contains(keyword) || roadAdd.contains(keyword) || jibunAdd.contains(keyword)) {
+                                    String address;
+
+                                    if (roadAdd.equals(" ")) {
+                                        address = jibunAdd + "/" + latitude + "/" + longitude;
+                                    } else {
+                                        address = roadAdd + "/" + latitude + "/" + longitude;
+                                    }
+
+                                    address_hash.put(name, address);
+                                }
                             }
 
 
