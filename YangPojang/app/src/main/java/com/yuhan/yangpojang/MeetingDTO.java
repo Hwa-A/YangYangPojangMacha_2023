69819e7f3399d63815e897768e7ee2a080c1bb3c
성은 <whtjddms2510@naver.com>
2023-10-15 23:10:23 +0900

    번개 데이터 삽입(미완, 진행중)

--- YangPojang/app/src/main/java/com/yuhan/yangpojang/MeetingDTO.java
+++ YangPojang/app/src/main/java/com/yuhan/yangpojang/MeetingDTO.java
@@ -1,82 +1,68 @@
 package com.yuhan.yangpojang;
-
+// pch: pojangmacha
 public class MeetingDTO {
-    String uid;     // 회원 ID
-    String pchName;     // 포차 이름
-    String title;       // 번개 소개글
-    String meetDate;        // 번개 날짜
-    String date;            // 번개 작성 날짜
+    String uid;             // 회원 ID
+    String pchName;         // 포차 이름
+    String title;           // 번개 소개글
+    String date;            // 번개 날짜
     String time;            // 번개 시간
-    double maxMember;          // 번개 인원
+    String writeDate;       // 번개 작성 날짜
+    double maxMember;       // 번개 정원
 
     // 생성자
     public MeetingDTO(String uid, String pchName) {
         this.uid = uid;
         this.pchName = pchName;
         this.title = "";
-        this.meetDate = "";
         this.date = "";
         this.time = "";
+        this.writeDate = "";
         this.maxMember = 0;
     }
-
-    // getter
-
-    public String getUid() {
-        return uid;
-    }
-
-    public String getPchName() {
-        return pchName;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getMeetDate() {
-        return meetDate;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public double getMaxMember() {
-        return maxMember;
-    }
-
 
     // setter
     public void setUid(String uid) {
         this.uid = uid;
     }
-
     public void setPchName(String pchName) {
         this.pchName = pchName;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
-    public void setMeetDate(String meetDate) {
-        this.meetDate = meetDate;
-    }
-
     public void setDate(String date) {
         this.date = date;
     }
-
     public void setTime(String time) {
         this.time = time;
     }
-
+    public void setWriteDate(String writeDate) {
+        this.writeDate = writeDate;
+    }
     public void setMaxMember(double maxMember) {
         this.maxMember = maxMember;
+    }
+
+    // getter
+    public String getUid() {
+        return uid;
+    }
+    public String getPchName() {
+        return pchName;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDate() {
+        return date;
+    }
+    public String getTime() {
+        return time;
+    }
+    public String getWriteDate() {
+        return writeDate;
+    }
+    public double getMaxMember() {
+        return maxMember;
     }
 }
--- YangPojang/app/src/main/java/com/yuhan/yangpojang/MeetingwriteActivity.java
+++ YangPojang/app/src/main/java/com/yuhan/yangpojang/MeetingwriteActivity.java
@@ -6,6 +6,9 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,20 +26,17 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-// met: meeting
+// meet: meeting
+// pch: pojangmacha
 public class MeetingwriteActivity extends AppCompatActivity {
-    MeetingDTO meetingDTO;          // 번개 객체
-    Button metRegisterBtn;          // 번개 등록 버튼
-    Button metCancelBtn;            // 번개 등록 취소
-    EditText metTitleEdt;           // 번개 소개글
-    EditText metMaxMember;          // 번개 정원
-    ImageView metDateImgv;          // 번개 날짜 버튼 대용
-    TextView metDateTv;             // 번개 날짜
-    TextView metTimeTv;             // 번개 시간
-    Calendar calendar;
-    DatePickerDialog.OnDateSetListener callbackMethod;
-    TimePickerDialog.OnTimeSetListener callbackTime;
-    private DatabaseReference metDatabase;      // DB 참조
+    MeetingDTO meetDTO;              // 번개 객체
+    EditText meetMaxEdt;          // 번개 정원
+    TextView meetDateTv;             // 번개 날짜
+    TextView meetTimeTv;             // 번개 시간
+    Button meetRegisterBtn;          // 번개 등록 버튼
+    Button meetCancelBtn;            // 번개 취소 버튼
+    // TimePickerDialog.OnTimeSetListener callbackTime;
+    private DatabaseReference meetDatabase;      // DB
 
 
     @Override
@@ -47,86 +49,124 @@ public class MeetingwriteActivity extends AppCompatActivity {
         String pchName = intent.getStringExtra("pchName");      // 포차 이름
         String uid = intent.getStringExtra("uid");              // 회원 ID
 
-        // 객체 생성 및 초기화
-        meetingDTO = new MeetingDTO(uid, pchName);
-        metDatabase = FirebaseDatabase.getInstance().getReference();
-        TextView pchNameTv = findViewById(R.id.tv_meetingwrite_pochaName);  // 상단의 포차 이름
-        metRegisterBtn = findViewById(R.id.btn_meetingwrite_register);
-        metCancelBtn = findViewById(R.id.btn_meetingwrite_cancel);
-        metTitleEdt = findViewById(R.id.edt_meetingwrite_title);
-        metMaxMember = findViewById(R.id.edt_meetingwrite_member);
-        metDateImgv = findViewById(R.id.imgv_meetingwrite_calenderButton);
-        metDateTv = findViewById(R.id.tv_meetingwrite_date);
-        metTimeTv = findViewById(R.id.tv_meetingwrite_time);
-        calendar = Calendar.getInstance();
-        callbackMethod = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                meetingDTO.setMeetDate(year+"/"+(month+1)+"/"+dayOfMonth);
-                metDateTv.setText(year+"/"+(month+1)+"/"+dayOfMonth);
-            }
-        };
-
-        callbackTime = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                meetingDTO.setTime(hourOfDay+"시 "+minute+"분");
-                metTimeTv.setText(hourOfDay+"시 "+minute+"분");
-            }
-        };
-
         // 상단의 포차 이름 설정
+        TextView pchNameTv = findViewById(R.id.tv_meetingwrite_pochaName);
         pchNameTv.setText(pchName);
 
-        // 리스너 연결
-        // 버튼 클릭 시, 번개 시간 선택(다이어로그로 구현)
-        metTimeTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // spinner 모드로 TimePickerDialog 구현
-                TimePickerDialog timePickerDialog = new TimePickerDialog(v.getContext(), android.R.style.Theme_Holo_Light_Dialog_NoActionBar, callbackTime,
-                        // is24Hour가 true: AM/PM의 12시간 모드, false:24시간 모드
-                        calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);
-                timePickerDialog.show();
-            }
-        });
-        // 버튼 클릭 시, 번개 날짜 선택(다이어로그로 구현)
-        metDateImgv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 다이어로그 생성 및 현재 날짜로 초기화
-                DatePickerDialog datePickerDialog = new DatePickerDialog(v.getContext(), callbackMethod, calendar.get(Calendar.YEAR),
-                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-                // 화면에 띄움
-                datePickerDialog.show();
-            }
-        });
-        // 버튼 클릭 시, 번개 등록
-        metRegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 번개 작성 시간 구하기
-                long now = System.currentTimeMillis();
-                Date date = new Date(now);
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-                meetingDTO.setDate(dateFormat.format(date));
-                // 번개 소개글
-                String titleData = metTitleEdt.getText().toString();
-                meetingDTO.setTitle(titleData);
-                // 번개 정원 구하기
-                double maxMemberData = Double.parseDouble(metMaxMember.getText().toString());
-                meetingDTO.setMaxMember(maxMemberData);
-                // DB에 저장
-                metDatabase.child("meetings").push().setValue(meetingDTO);
-                onBackPressed();
-            }
-        });
+        // 객체 생성 및 초기화 코드
+        // 번개 객체 필드 관련
+        meetDTO = new MeetingDTO(uid, pchName);
+        meetDateTv = findViewById(R.id.tv_meetingwrite_date);
+        meetTimeTv = findViewById(R.id.tv_meetingwrite_time);
+        meetMaxEdt = findViewById(R.id.edt_meetingwrite_member);
+        TextInputLayout titleInputLayout = findViewById(R.id.txtLay_meetingwrite_titleContainer);   // 번개 소개글 컨테이너
+        EditText meetTitleEdt = findViewById(R.id.edt_meetingwrite_title);
+        // 번개 등록/취소 버튼 관련
+        meetRegisterBtn = findViewById(R.id.btn_meetingwrite_register);
+        meetCancelBtn = findViewById(R.id.btn_meetingwrite_cancel);
+        // firebase 관련
+        // meetDatabase = FirebaseDatabase.getInstance().getReference();
+        // editText에 공백 입력 여부 확인 - 공백 시, error message 출력
+        emptyError(meetTitleEdt, titleInputLayout);
+
+
+//
+//        calendar = Calendar.getInstance();
+//        callbackMethod = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                meetingDTO.setMeetDate(year+"/"+(month+1)+"/"+dayOfMonth);
+//                metDateTv.setText(year+"/"+(month+1)+"/"+dayOfMonth);
+//            }
+//        };
+//
+//        callbackTime = new TimePickerDialog.OnTimeSetListener() {
+//            @Override
+//            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                meetingDTO.setTime(hourOfDay+"시 "+minute+"분");
+//                metTimeTv.setText(hourOfDay+"시 "+minute+"분");
+//            }
+//        };
+//
+//
+//
+//        // 리스너 연결
+//        // 버튼 클릭 시, 번개 시간 선택(다이어로그로 구현)
+//        metTimeTv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // spinner 모드로 TimePickerDialog 구현
+//                TimePickerDialog timePickerDialog = new TimePickerDialog(v.getContext(), android.R.style.Theme_Holo_Light_Dialog_NoActionBar, callbackTime,
+//                        // is24Hour가 true: AM/PM의 12시간 모드, false:24시간 모드
+//                        calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);
+//                timePickerDialog.show();
+//            }
+//        });
+//        // 버튼 클릭 시, 번개 날짜 선택(다이어로그로 구현)
+//        metDateImgv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // 다이어로그 생성 및 현재 날짜로 초기화
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(v.getContext(), callbackMethod, calendar.get(Calendar.YEAR),
+//                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+//                // 화면에 띄움
+//                datePickerDialog.show();
+//            }
+//        });
+//        // 버튼 클릭 시, 번개 등록
+//        metRegisterBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // 번개 작성 시간 구하기
+//                long now = System.currentTimeMillis();
+//                Date date = new Date(now);
+//                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+//                meetingDTO.setDate(dateFormat.format(date));
+//                // 번개 소개글
+//                String titleData = metTitleEdt.getText().toString();
+//                meetingDTO.setTitle(titleData);
+//                // 번개 정원 구하기
+//                double maxMemberData = Double.parseDouble(metMaxMember.getText().toString());
+//                meetingDTO.setMaxMember(maxMemberData);
+//                // DB에 저장
+//                metDatabase.child("meetings").push().setValue(meetingDTO);
+//                onBackPressed();
+//            }
+//        });
         // 버튼 클릭 시, 번개 작성 취소
-        metCancelBtn.setOnClickListener(new View.OnClickListener() {
+        meetCancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();       // 현재 Activity 종료
             }
         });
     }
+    // editText에 공백 입력 시, error message 출력 코드
+    private void emptyError(EditText edt, TextInputLayout txtLayout){
+        edt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // text 입력 전 실행되는 메서드
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // text 입력과 동시에 실행되는 메서드
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // text 입력 후 실행되는 메서드: text가 변경된 것을 통보할 때 사용
+                // TextUtils.isEmpty(): 라이브러리에서 제공하는 'null or 공백' 체크 함수
+                if(TextUtils.isEmpty(s.toString())){
+                    // text가 공백인 경우
+                    txtLayout.setError("문자를 입력해주세요");
+                }else{
+                    // text가 공백이 아닌 경우
+                    txtLayout.setError(null);
+                }
+            }
+        });
+    }
+
 }
