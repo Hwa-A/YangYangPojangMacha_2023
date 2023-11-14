0654120434c18f1c81fa2cc0245f0e1bf7c76305
lee <you9718@naver.com>
2023-11-14 21:23:41 +0900

    account.java 변경

--- YangPojang/app/src/main/java/com/yuhan/yangpojang/mypage/account/accountPage.java
+++ YangPojang/app/src/main/java/com/yuhan/yangpojang/mypage/account/accountPage.java
@@ -227,6 +227,7 @@ public class accountPage extends AppCompatActivity {
                 .setPositiveButton("네", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int i) {
+                        signOut();
                         deleteAccount();
                         Intent intent = new Intent(getApplicationContext(), SplashImage.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -269,15 +270,14 @@ public class accountPage extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-              user.delete();
-              mGoogleSignInClient.revokeAccess();
-              storageReference.delete();
+                user.delete();
+                mGoogleSignInClient.revokeAccess();
+                storageReference.delete();
             }
         });
     }
 
 
 }
-
 
 
